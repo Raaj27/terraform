1cc936de8250625caeb99becc299276d21e3fd50
Raaj27 <raajkmr7@gmail.com>
2021-02-09 18:07:56 +0530

    Add files via upload

---
+++ main.tf
@@ -0,0 +1,26 @@
+
+
+data "aws_security_group" "IGT_Ansible" {
+  name= "IGT_Ansible"
+}
+
+
+
+
+resource "aws_instance" "sql2019" {
+  ami           = "ami-0ce37705b0668418d"
+  instance_type = "t2.micro"
+  key_name = "windowssql"
+ vpc_security_group_ids = [data.aws_security_group.IGT_Ansible.id]
+}
+
+
+
+resource "aws_instance" "centOS7" {
+  ami           = "ami-005c06c6de69aee84"
+  instance_type = "t2.micro"
+  key_name = "windowssql"
+ vpc_security_group_ids = [data.aws_security_group.IGT_Ansible.id]
+}
+
+                                  
