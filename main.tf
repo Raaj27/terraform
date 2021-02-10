7e9d3b0ce6aa1482e79b0fd046cf8c3eef169744
Raaj27 <raajkmr7@gmail.com>
2021-02-10 06:56:57 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,13 +1,11 @@
 
 
-provider "aws" {
-  region= var.region
-  access_key = var.aws_access_key
-  secret_key = var.aws_secret_key
-  }
 
-variable "aws_access_key" {}
-variable "aws_secret_key" {}
+  region= var.region
+  
+  
+
+
 
 data "aws_security_group" "IGT_Ansible" {
   name= "IGT_Ansible"
