50abf2de15a4ebf301b7d0ae648b2b41d640a99f
Raaj27 <raajkmr7@gmail.com>
2021-02-16 15:31:12 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -51,7 +51,7 @@ resource "aws_instance" "centOS7" {
   ami           = "ami-005c06c6de69aee84"
   instance_type = "t2.micro"
   key_name = var.key_name
- vpc_security_group_ids = [data.aws_security_group.IGT_Ansible.id]
+ vpc_security_group_ids = [data.aws_security_group.default.id]
     
 }
 
