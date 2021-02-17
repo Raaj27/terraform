afa8eb8d707336e5a382796c53cd2f2e26e3541d
Raaj27 <raajkmr7@gmail.com>
2021-02-17 12:56:27 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -18,7 +18,7 @@ data "aws_security_group" "default" {
 }
 
 variable "region" {
-  default = "us-west-2"
+  default = "us-west-1"
 }
 variable "key_name" {
   default = "windowssql"
@@ -26,7 +26,7 @@ variable "key_name" {
 
 
 resource "aws_instance" "centOS7" {
-  ami           = "ami-005c06c6de69aee84"
+  ami           = "ami-024d79f468ff716c1"
   instance_type = "t2.micro"
   key_name = var.key_name
  vpc_security_group_ids = [data.aws_security_group.default.id]
